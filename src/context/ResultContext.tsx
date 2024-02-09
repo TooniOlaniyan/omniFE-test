443f3db88552d0b7a139b888d1e6b64c8783a981
TooniOlaniyan <asalamlatif@gmail.com>
2024-02-09 10:32:56 +0100

    changes

--- src/context/ResultContext.tsx
+++ src/context/ResultContext.tsx
@@ -1,45 +1,45 @@
-// ResultContext.js
+// // ResultContext.js
 
-import  { createContext, useContext , useState , ReactNode } from "react";
+// import  { createContext, useContext , useState , ReactNode } from "react";
 
-const ResultContext = createContext('');
+// const ResultContext = createContext('');
 
 
 
-export const ResultProvider = (
-  {children} : {children: ReactNode}
-) => {
-  const [resultData, setResultData] = useState('');
-  const handleDownload = async (id: string) => {
-    try {
-      let url = `https://test.omniswift.com.ng/api/viewResult/${id}`;
-      let requestOptions = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-      const response = await fetch(url, requestOptions);
-      const data = await response.json();
-      setResultData(data);
-      return data;
-    } catch (error) {
-      console.error("Error downloading results:", error);
-      return null;
-    }
-  };
-  const value = {
-    resultData,
-    handleDownload
-  }
+// export const ResultProvider = (
+//   {children} : {children: ReactNode}
+// ) => {
+//   const [resultData, setResultData] = useState('');
+//   const handleDownload = async (id: string) => {
+//     try {
+//       let url = `https://test.omniswift.com.ng/api/viewResult/${id}`;
+//       let requestOptions = {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       };
+//       const response = await fetch(url, requestOptions);
+//       const data = await response.json();
+//       setResultData(data);
+//       return data;
+//     } catch (error) {
+//       console.error("Error downloading results:", error);
+//       return null;
+//     }
+//   };
+//   const value = {
+//     resultData,
+//     handleDownload
+//   }
 
-  return (
-    <ResultContext.Provider value={value}>
-      {children}
-    </ResultContext.Provider>
-  );
-};
+//   return (
+//     <ResultContext.Provider value={value}>
+//       {children}
+//     </ResultContext.Provider>
+//   );
+// };
 
-export const useResultContext = () => {
-  return useContext(ResultContext);
-};
+// export const useResultContext = () => {
+//   return useContext(ResultContext);
+// };

ad75182fb9c079c5198be04beff2912b9d5300b0
TooniOlaniyan <asalamlatif@gmail.com>
2024-02-09 10:36:55 +0100

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ResultProvider } from './context/ResultContext.tsx'
+// import { ResultProvider } from './context/ResultContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ResultProvider>
+    {/* <ResultProvider> */}
     <App />
-    </ResultProvider>
+    {/* </ResultProvider> */}
   </React.StrictMode>,
 )
